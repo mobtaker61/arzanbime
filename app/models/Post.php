38bed6276a4c04b5ef819c3f0aeb5a8bafaa5678
mobtaker61 <m.totonchi@hotmail.com>
2024-06-06 07:51:13 +0300

    rd5

--- app/models/Post.php
+++ app/models/Post.php
@@ -3,31 +3,61 @@ class Post extends Model {
     public function getAllPosts() {
         $stmt = $this->db->prepare("SELECT * FROM post WHERE is_active = 1");
         $stmt->execute();
-        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        $result = $this->fetchAssoc($stmt);
+        $stmt->close();
+        return $result;
     }
 
     public function getPostById($id) {
         $stmt = $this->db->prepare("SELECT * FROM post WHERE id = ?");
         $stmt->bind_param('i', $id);
         $stmt->execute();
-        return $stmt->get_result()->fetch_assoc();
+        $result = $this->fetchAssoc($stmt);
+        $stmt->close();
+        return !empty($result) ? $result[0] : null;
     }
 
     public function createPost($data) {
         $stmt = $this->db->prepare("INSERT INTO post (post_type, title, caption, full_body, image, is_active) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->bind_param('issssi', $data['post_type'], $data['title'], $data['caption'], $data['full_body'], $data['image'], $data['is_active']);
-        return $stmt->execute();
+        $result = $stmt->execute();
+        $stmt->close();
+        return $result;
     }
 
     public function updatePost($id, $data) {
         $stmt = $this->db->prepare("UPDATE post SET post_type = ?, title = ?, caption = ?, full_body = ?, image = ?, is_active = ? WHERE id = ?");
         $stmt->bind_param('issssii', $data['post_type'], $data['title'], $data['caption'], $data['full_body'], $data['image'], $data['is_active'], $id);
-        return $stmt->execute();
+        $result = $stmt->execute();
+        $stmt->close();
+        return $result;
     }
 
     public function deletePost($id) {
         $stmt = $this->db->prepare("DELETE FROM post WHERE id = ?");
         $stmt->bind_param('i', $id);
-        return $stmt->execute();
+        $result = $stmt->execute();
+        $stmt->close();
+        return $result;
+    }
+
+    private function fetchAssoc($stmt) {
+        $stmt->store_result();
+        $variables = [];
+        $data = [];
+        $meta = $stmt->result_metadata();
+        while ($field = $meta->fetch_field()) {
+            $variables[] = &$data[$field->name];
+        }
+        call_user_func_array([$stmt, 'bind_result'], $variables);
+        $results = [];
+        while ($stmt->fetch()) {
+            $row = [];
+            foreach ($data as $key => $val) {
+                $row[$key] = $val;
+            }
+            $results[] = $row;
+        }
+        return $results;
     }
 }
