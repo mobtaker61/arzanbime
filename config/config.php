0165aa74cea17d5236d30dcd8b0b6f676011548a
mobtaker61 <m.totonchi@hotmail.com>
2024-06-27 16:20:52 +0300

    Stop tracking config.php and add to .gitignore

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'db' => [
-        'host' => '188.132.198.82',
-        'user' => 'arzanbime_mdb',
-        'pass' => 'k0moBdbDsmL%',
-        'name' => 'arzanbime_main',
-    ],
-];
