31c5a8350e633dcc49860650754b1bde5cb4e791
mobtaker61 <m.totonchi@hotmail.com>
2024-07-04 10:14:41 +0300

    fcm-6

--- app/views/layouts/public/bodycode.php
+++ app/views/layouts/public/bodycode.php
@@ -1,30 +1,4 @@
 <script type="module">
-    // Import the functions you need from the SDKs you need
-    import {initializeApp} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-    import {getMessaging, getToken} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-messaging.js";
-    import {getAnalytics} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-        apiKey: "AIzaSyCkeH7FmK0WRuH-jdn8PABTtxkkYlwE7UI",
-        authDomain: "arzanbime.firebaseapp.com",
-        projectId: "arzanbime",
-        storageBucket: "arzanbime.appspot.com",
-        messagingSenderId: "532740741849",
-        appId: "1:532740741849:web:8582c7312668cb0c593cc8",
-        measurementId: "G-Q5WLDR1WW4"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    // Get registration token. Initially this makes a network call, once retrieved
-    // subsequent calls to getToken will return from cache.
-    const messaging = getMessaging();
-
     // Request permission to send notifications
     messaging.requestPermission()
         .then(function() {
